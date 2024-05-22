66f26476ee2ec782df090c943774ad4e1be161a2
Shaswata <121812904+ShaswataG@users.noreply.github.com>
2024-05-23 01:27:11 +0530

    icon fix

--- src/App.js
+++ src/App.js
@@ -4,38 +4,75 @@ import Navbar from './components/Navbar'
 import Platforms from './components/Platforms'
 import About from './components/About'
 import Skills from './components/Skills'
-import Projects from  './components/Projects'
+import Projects from './components/Projects'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
 
 export default function App() {
-  const [nav, setNav] = React.useState(false)
+	const [nav, setNav] = React.useState(false)
 
-  const toggleNav = () => {
-    setNav(nav => !nav)
-  }
+	const toggleNav = () => {
+		setNav(nav => !nav)
+	}
 
-
-
-
-  return (
-    <>
-      <div className='app'>
-        <Navbar navStatus={nav} toggleNav={toggleNav}/>
-        <Platforms />
-        <main className='main'>
-        <button style={{borderRadius: "50%", border: "0px", padding: "0px", width: "40px", height: "40px", position: "fixed", top: "330px", left: "0px"}} className="toggle-nav-button" onClick={toggleNav}><img width="40px" src="https://shaswatag.github.io/PRODIGY_WD_04/images/arrow-right-circle-fill.svg"/></button>
-        <button style={{borderRadius: "50%", border: "0px", padding: "0px", width: "40px", height: "40px", position: "fixed", top: "10px", right: "10px"}} className="toggle-nav-button" onClick={toggleNav}  ><img src={`nav ? "https://shaswatag.github.io/PRODIGY_WD_04/images/list.svg" : "https://shaswatag.github.io/PRODIGY_WD_04/images/x.svg"`} /></button>
-          <Home />
-          <About />
-          <Skills />
-          <Projects />
-          <Contact />
-          {/* <Footer /> */}
-        </main>
-        {/* <Footer /> */}
-      </div>
-      <Footer />
-    </>
-  )
+	return (
+		<>
+			<div className="app">
+				<Navbar
+					navStatus={nav}
+					toggleNav={toggleNav}
+				/>
+				<Platforms />
+				<main className="main">
+					<button
+						style={{
+							borderRadius: '50%',
+							border: '0px',
+							padding: '0px',
+							width: '40px',
+							height: '40px',
+							position: 'fixed',
+							top: '330px',
+							left: '0px',
+						}}
+						className="toggle-nav-button"
+						onClick={toggleNav}>
+						<img
+							width="40px"
+							src="https://shaswatag.github.io/PRODIGY_WD_04/images/arrow-right-circle-fill.svg"
+						/>
+					</button>
+					<button
+						style={{
+							borderRadius: '50%',
+							border: '0px',
+							padding: '0px',
+							width: '40px',
+							height: '40px',
+							position: 'fixed',
+							top: '10px',
+							right: '10px',
+						}}
+						className="toggle-nav-button"
+						onClick={toggleNav}>
+						<img
+							src={
+								nav
+									? 'https://shaswatag.github.io/PRODIGY_WD_04/images/list.svg'
+									: 'https://shaswatag.github.io/PRODIGY_WD_04/images/x.svg'
+							}
+						/>
+					</button>
+					<Home />
+					<About />
+					<Skills />
+					<Projects />
+					<Contact />
+					{/* <Footer /> */}
+				</main>
+				{/* <Footer /> */}
+			</div>
+			<Footer />
+		</>
+	)
 }
